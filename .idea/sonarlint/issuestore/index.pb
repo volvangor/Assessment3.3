2c3ab9f71e7350b8fd8287f80bc9649cf16c90c7
Volvangor <2335444+volvangor@users.noreply.github.com>
2021-08-04 08:42:04 +1000

    window resize

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,10 @@
 
 A
 package-lock.json,f/a/fa288d1472d29beccb489a676f68739ad365fc47
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ import * as THREE from "three";
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import {GUI} from "three/examples/jsm/libs/dat.gui.module.js";
 
+function rand_normal() {
+    let u = 0, v = 0;
+    while (u === 0) u = Math.random();
+    while (v === 0) v = Math.random();
+    return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+}
+
 function getPoint() {
-    let u = Math.random();
-    let v = Math.random();
-    let theta = u * 2.0 * Math.PI;
-    let phi = Math.acos(2.0 * v - 1.0);
-    let r = Math.cbrt((Math.random() * 100000000000));
-    let sinTheta = Math.sin(theta);
-    let cosTheta = Math.cos(theta);
-    let sinPhi = Math.sin(phi);
-    let cosPhi = Math.cos(phi);
-    let x = r * sinPhi * cosTheta;
-    let y = r * sinPhi * sinTheta;
-    let z = r * cosPhi;
-    return {x: x, y: y, z: z};
+    let u = Math.random() * 100000000000;
+    let x1 = rand_normal();
+    let x2 = rand_normal();
+    let x3 = rand_normal();
+
+    let mag = Math.sqrt(x1 * x1 + x2 * x2 + x3 * x3);
+    x1 /= mag;
+    x2 /= mag;
+    x3 /= mag;
+
+    // Math.cbrt is cube root
+    let c = Math.cbrt(u);
+
+    return {x: x1 * c, y: x2 * c, z: x3 * c};
 }
 
 class ColorGUIHelper {
@@ -34,7 +42,7 @@ class ColorGUIHelper {
 }
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000000);
+const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.01, 1000000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -43,19 +51,15 @@ const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.z = 50;
 controls.update();
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener("resize", onWindowResize, false);
 
-function onWindowResize(){
-
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 const vertices = [];
-vertices.push(0, 0, 0);
 for (let i = 0; i < 50000; i++) {
     const coord = getPoint();
     vertices.push(coord.x, coord.y, coord.z);
@@ -67,22 +71,37 @@ const material = new THREE.PointsMaterial({color: "#ffffff"});
 material.size = 5;
 const points = new THREE.Points(geometry, material);
 scene.add(points);
-let menu_params = {rotation_rate: 0.00001};
 
+
+let menu_params = {
+};
+
+function updateCamera() {
+    camera.updateProjectionMatrix();
+}
 
 const gui = new GUI();
 const pointsFolder = gui.addFolder("Points");
 pointsFolder.add(points.material, "size", 0.001, 10);
-pointsFolder.addColor(new ColorGUIHelper(material, "color"), "value")
+pointsFolder.addColor(new ColorGUIHelper(material, "color"), "value");
+pointsFolder.add(points.position, "x", -5000, 5000);
+pointsFolder.add(points.position, "y", -5000, 5000);
+pointsFolder.add(points.position, "z", -5000, 5000);
 pointsFolder.open();
 const viewFolder = gui.addFolder("View");
-viewFolder.add(menu_params, "rotation_rate", 0, 0.001);
+viewFolder.add(camera, "fov", 30, 175).onChange(updateCamera);
+viewFolder.add(menu_params, "rotation_rate_x", 0, 0.001);
+viewFolder.add(menu_params, "rotation_rate_y", 0, 0.001);
+viewFolder.add(menu_params, "rotation_rate_z", 0, 0.001);
 viewFolder.open();
+
 
 const animate = function () {
     requestAnimationFrame(animate);
-    points.rotation.x += menu_params.rotation_rate;
-    points.rotation.y += menu_params.rotation_rate;
+    points.rotation.x += menu_params.rotation_rate_x;
+    points.rotation.y += menu_params.rotation_rate_y;
+    points.rotation.z += menu_params.rotation_rate_z;
+    //console.log(camera.position)
 
     controls.update();
     renderer.render(scene, camera);
